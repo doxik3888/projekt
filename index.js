5d48259b86215d5ec30e363629ea243b85d55fda
doxik3888 <ddemochkin03@gmail.com>
2025-06-01 17:06:31 +0300

    jjjjjjjhg

--- index.js
+++ index.js
@@ -7,3 +7,35 @@ clicker.addEventListener("click", () => {
         likes.innerHTML = +likes.innerHTML + 1  
     }
 })
+const progress = document.querySelector('.xv1-progress')    
+function initPlayer() {
+    const btn = document.querySelector('.xv1-playbtn')
+    const audio = document.getElementById('audioPlayer')
+    btn.addEventListener('click',() => {
+    if (audio.paused) {
+        audio.play()
+        btn.innerHTML = '<i class="fas fa-pause"></i>'
+        document.documentElement.style.setProperty('--animationplaystate', 'running')
+    } else {
+        audio.pause()
+        btn.innerHTML = '<i class="fas fa-play"></i>'
+        document.documentElement.style.setProperty('--animationplaystate', 'paused')
+    }
+    })
+    const time = document.querySelector('.xv1-time')
+    audio.addEventListener('timeupdate', () => {
+        progress.style.width = `${audio.currentTime / audio.duration * 100}%`
+        let minutes = Math.floor(audio.currentTime / 60)
+        let seconds = Math.floor(audio.currentTime % 60).toString().padStart(2, "0")
+        // let maxMinutes = Math.floor(audio.currentTime / 60)
+        // let maxSeconds = Math.floor(audio.currentTime % 60).toString().padStart(2, "0") ${maxMinutes}:${maxSeconds}
+         time.innerHTML = `${minutes}:${seconds} / 2:52`
+    })
+    document.querySelector('.xv1-progressbar').addEventListener('click', (e) => {
+        const rect = e.target.getBoundingClientRect()
+        const pos = (e.clientX - rect.left) / rect.width
+        audio.currentTime = pos * audio.duration
+    })
+}
+
+document.addEventListener("DOMContentLoaded", initPlayer)
