f0f00789bde0145eb8ea6b4b1bab7849d5dd5cbb
doxik3888 <153354996+doxik3888@users.noreply.github.com>
2025-06-01 17:21:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+function initGallery(){
+    let gallery = document.querySelector(".gallery")
+    let galleryItem = document.querySelectorAll(".gallery-item")
+    gallery.style.setProperty("--total-item", galleryItem.length)
+
+    gallery.addEventListener("click", (event) =>{
+        let clicked = event.target.closest(".gallery-item")
+        if (!clicked || clicked.classList.contains("active")) return
+
+        galleryItem.forEach((item) => {
+            item.classList.remove("active")
+        })
+        clicked.classList.add("active")
+    })
+
+}
+
+document.addEventListener("DOMContentLoaded",initGallery)
 let clicker = document.querySelector(".clicker")
 let likes = document.querySelector(".amount")
 clicker.addEventListener("click", () => {
